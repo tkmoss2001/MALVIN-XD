3f758e32afdd42f40672b1b73277e520e7ef1989
tkmoss2001 <kuthira9@gmail.com>
2025-07-25 09:58:44 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: " malvin~9qUF0LYa#q1OBZB_zkAUPHcO1EI8ft965QrGIZ0APgnnjznVRuc4",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  PREFIX: "prvit" // prefix (e.g., ., /, !, *)
 };
